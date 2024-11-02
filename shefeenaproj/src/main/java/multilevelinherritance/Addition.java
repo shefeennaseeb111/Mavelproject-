622a75caf778e12f07c5c1fc7ae75a4cb78b671e
NaSeEb <NaSeEb@DESKTOP-QTFR6NJ>
2024-11-02 10:21:34 -0700

    mavel

--- shefeenaproj/src/main/java/multilevelinherritance/Addition.java
+++ shefeenaproj/src/main/java/multilevelinherritance/Addition.java
@@ -3,7 +3,7 @@ package multilevelinherritance;
 public class Addition {
 	int a=90;
 	int b=50;
-	int y;
+
 	public void add()
 	{
 		int c=a+b;
---
+++ shefeenaproj/src/main/java/multilevelinherritance/Loop.java
@@ -0,0 +1,18 @@
+package multilevelinherritance;
+
+public class Loop
+{
+	public static void main(String[] args)
+	{
+		
+		{
+			
+			for(int a=0;a<=10;a++)
+			{
+				System.out.println(a);
+			}
+			}
+	}
+}
+
+
